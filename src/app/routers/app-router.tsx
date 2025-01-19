7789e4a2f8e3903b7fd37497d221b2033677ad40
HoJin9622 <kiss0104040@gmail.com>
2025-01-19 22:57:17 +0900

    feat: 홈페이지 레이아웃 생성
    PROF-27

--- src/app/routers/app-router.tsx
+++ src/app/routers/app-router.tsx
@@ -1,11 +1,14 @@
 import { HomePage } from '@/pages/home';
+import { Layout } from '@/shared/ui/layout';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/shared/ui/layout.tsx
@@ -0,0 +1,53 @@
+import { Link, Outlet } from 'react-router';
+
+export function Layout() {
+  return (
+    <>
+      <header className="border-b-[1px] border-[#dde1e6] py-4 px-4 flex items-center justify-between">
+        <div className="flex items-center gap-24">
+          <Link to="/">
+            <h1 className="font-bold text-[#fbbf24] text-[24px]">ProfitKey</h1>
+          </Link>
+          <input className="w-72 h-14 border-[#cfcfcf] border-[1px] rounded-2xl py-4 px-5" />
+        </div>
+
+        <div className="flex items-center gap-16">
+          <div className="flex items-center gap-10">
+            <Link
+              className="font-medium text-[#36398c] text-[18px]"
+              to="/stocks"
+            >
+              종목분석
+            </Link>
+            <Link className="font-medium text-[#36398c] text-[18px]" to="/news">
+              뉴스
+            </Link>
+            <Link
+              className="font-medium text-[#36398c] text-[18px]"
+              to="/community"
+            >
+              커뮤니티
+            </Link>
+            <Link
+              className="font-medium text-[#36398c] text-[18px]"
+              to="/topics"
+            >
+              공지사항/FAQ
+            </Link>
+          </div>
+          <div>
+            <Link
+              className="font-medium text-[#36398c] text-[18px]"
+              to="/login"
+            >
+              로그인
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
