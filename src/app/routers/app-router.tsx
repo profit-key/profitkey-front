b219b3074064004083a155526718f4bcea5f6be8
nayeongdev <nayeongkim.dev@gmail.com>
2025-02-06 00:57:08 +0900

    feat: 주식 목록 페이지 구조 추가

--- src/app/routers/app-router.tsx
+++ src/app/routers/app-router.tsx
@@ -1,6 +1,7 @@
 import { AuthPage } from '@/pages/auth';
 import { HomePage } from '@/pages/home';
 import { LoginPage } from '@/pages/login';
+import { StockListPage } from '@/pages/stock-list';
 import { Layout } from '@/shared/ui';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
@@ -12,6 +13,7 @@ export function AppRouter() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/auth" element={<AuthPage />} />
+          <Route path="/stocks" element={<StockListPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/stock-list/ui/stock-list-page.tsx
@@ -0,0 +1,3 @@
+export function StockListPage() {
+  return <div>StockList</div>;
+}
