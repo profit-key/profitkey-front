e7d27305dea64665c3b002fc354468db944223a9
nayeongdev <nayeongkim.dev@gmail.com>
2025-02-15 18:19:52 +0900

    feat: 댓글 좋아요 버튼 마크업

--- src/pages/stock-detail/ui/comment.tsx
+++ src/pages/stock-detail/ui/comment.tsx
@@ -1,9 +1,22 @@
 import { type PropsWithChildren } from 'react';
 import { ProfileH } from './profile-h';
 import { formatDate } from './date.ts';
+import { HeartIcon } from './icon.tsx';
+import { useState } from 'react';
 
 export function Comment({ children }: PropsWithChildren) {
   const username: string = 'user';
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeButton() {
+    setIsLiked(!isLiked);
+    if (isLiked) {
+      setLikeCount((c) => c - 1);
+      return;
+    }
+    setLikeCount((c) => c + 1);
+  }
 
   return (
     <li className="flex flex-col gap-[10px]">
@@ -15,8 +28,17 @@ export function Comment({ children }: PropsWithChildren) {
         <div className="min-h-[50px] w-full rounded-[5px] bg-[#FFB40033] p-4">
           <p>{children}</p>
         </div>
+        <div className="flex">
+          <button onClick={handleLikeButton} className="flex gap-2 p-2">
+            {isLiked ? (
+              <HeartIcon className="fill-[#D94F70]" />
+            ) : (
+              <HeartIcon className="fill-neutral-300" />
+            )}
+            <span>{likeCount ? likeCount : null}</span>
+          </button>
+        </div>
       </div>
-      <div></div>
     </li>
   );
 }
