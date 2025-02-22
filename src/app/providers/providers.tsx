a977b4fe3a1d167485ea09377672fa9b72a81c89
nayeongdev <nayeongkim.dev@gmail.com>
2025-02-22 15:58:16 +0900

    feat: tanstack query devtool 추가

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { NuqsAdapter } from 'nuqs/adapters/react';
 
 import { type PropsWithChildren } from 'react';
@@ -9,6 +10,7 @@ export function Providers({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
       <NuqsAdapter>{children}</NuqsAdapter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
