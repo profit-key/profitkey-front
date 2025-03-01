3713faa1ea407d587a7ab0faaf37310c80afd55e
nayeongdev <nayeongkim.dev@gmail.com>
2025-03-02 03:40:33 +0900

    feat: 주식정보 테이블 및 AI코멘트 마크업

--- src/pages/stock-detail/ui/stock-analyst.tsx
+++ src/pages/stock-detail/ui/stock-analyst.tsx
@@ -1,7 +1,59 @@
+import { StockAnalysisTable } from './stock-analysis-table';
+
 export function StockAnalyst() {
+  const analysisData = {
+    yearHighPrice: 90000000,
+    yearLowPrice: 29000000,
+    per: 3000.22,
+    pbr: 500.0,
+    eps: 30000.0,
+    bps: 310000.0,
+    highPriceRatio: 90,
+    lowPriceRatio: -90,
+    dividend: 19000,
+    dividendYield: 120.0,
+  };
+
+  const aiComment =
+    '매출액 증가율(21.74%)은 299910보다 낮지만, 자기자본 증가율(46.63%)과 총자산 증가율(27.50%)이 긍정적임 수익성 지표에서 총자본 순이익율(4.81%), 자기자본 순이익율(11.69%), 매출액 순이익율(6.24%)이 양호하며, 299910의 수익성 지표가 모두 음수인 것과 대조됨 안정성 지표에서 부채 비율(109.58%)이 299910(806.53%)보다 낮고, 차입금 의존도(13.31%)도 양호함 유동 비율(151.64%)이 299910(86.72%)보다 높아 재무 건전성이 우수함';
+
   return (
-    <div>
-      <h3>종목분석</h3>
+    <div className="flex flex-col gap-16">
+      <section className="border-b border-black pb-8">
+        <h3 className="sr-only">종목 분석</h3>
+        <div className="flex gap-8">
+          <div className="flex-1">
+            <h4 className="sr-only">종목 테이블</h4>
+            <StockAnalysisTable data={analysisData} />
+          </div>
+          <div className="flex-1">
+            <h4 className="sr-only">종목 차트</h4>
+            <iframe
+              className="h-full w-full border-none"
+              src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_123&symbol=AAPL&interval=D&theme=light"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h3 className="text-3xl font-bold">AI 코멘트</h3>
+        <div className="min-h-[50px] w-full break-words rounded-[5px] bg-[#FFB40033] p-4">
+          {aiComment}
+        </div>
+      </section>
+
+      <section>
+        <h3 className="text-3xl font-bold">
+          재무지표 <small className="text-base text-[#6e6e6e]">(분기별)</small>
+        </h3>
+      </section>
+
+      <section>
+        <h3 className="text-3xl font-bold">투자사별 주식 분석</h3>
+        <h4 className="text-2xl font-bold">통계 한눈에 보기</h4>
+        <h4 className="text-2xl font-bold">투자사별 의견</h4>
+      </section>
     </div>
   );
 }
