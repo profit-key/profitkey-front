bcba146a6d455e64eabf8f72b8179d4d8f36217f
nayeongdev <nayeongkim.dev@gmail.com>
2025-03-29 23:23:08 +0900

    remove: stock list 삭제

--- src/app/routers/app-router.tsx
+++ src/app/routers/app-router.tsx
@@ -2,7 +2,6 @@ import { AuthPage } from '@/pages/auth';
 import { HelpPage } from '@/pages/help';
 import { HomePage } from '@/pages/home';
 import { LoginPage } from '@/pages/login';
-import { StockListPage } from '@/pages/stock-list';
 import { StockDetailPage } from '@/pages/stock-detail';
 import { Layout } from '@/shared/ui';
 import { BrowserRouter, Route, Routes } from 'react-router';
@@ -20,7 +19,6 @@ export function AppRouter() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/auth" element={<AuthPage />} />
-          <Route path="/stocks" element={<StockListPage />} />
           <Route path="/stocks/:stockCode" element={<StockDetailPage />} />
           <Route path="/help" element={<HelpPage />} />
           <Route
