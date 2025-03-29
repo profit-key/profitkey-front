94247f59fb2ee0579ba35f5966450e4d803c2763
HoJin9622 <kiss0104040@gmail.com>
2025-03-30 02:43:08 +0900

    feat: AI추천종목 등락율, 주가 정보 표시

---
+++ src/pages/home/ui/ai-opinion/stock-summary.tsx
@@ -0,0 +1,38 @@
+import { stockQueries } from '@/entities/stock';
+import { formatNumberWithCommas } from '@/shared/lib/number';
+import { Skeleton } from '@/shared/ui/skeleton';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+type Props = {
+  stockCode: string;
+};
+
+function StockSummary({ stockCode }: Props) {
+  const { data: stockSummary } = useSuspenseQuery(
+    stockQueries.summary(stockCode)
+  );
+
+  return (
+    <>
+      <div className="rounded-lg bg-[#D9001B33] px-1 py-3">
+        {stockSummary.changeRate}%
+      </div>
+      <span className="text-xl font-bold">
+        {formatNumberWithCommas(+stockSummary.price)}원
+      </span>
+    </>
+  );
+}
+
+function Loading() {
+  return (
+    <>
+      <Skeleton className="h-12 w-12" />
+      <Skeleton className="h-7 w-20" />
+    </>
+  );
+}
+
+StockSummary.Loading = Loading;
+
+export { StockSummary };
